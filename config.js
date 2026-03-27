16ffb4b00ad493fb38c96853ca404e9f73284aec
diasmillan727-lgtm <diasmillan727@gmail.com>
2026-03-26 23:28:31 -0600

    GataBotLite-MD ~ Gata Dios
    Updated the global owner list with new Bot numbers.

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ import axios from 'axios'
 import moment from 'moment-timezone'
 import { en, es, id, ar, pt, de, it } from './lib/idiomas/total-idiomas.js'
 
-// ES ➜ Agregué primero el número del Bot como prioridad
-// ES ➜ Si desea recibir reportes debe de usar los tres parámetros (Número, nombre y true)
-// EN ➜ Add the Bot number as priority first
-// EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
+// ES ➜ 7271107607
+
+// EN ➜ 7331446115
+
 global.owner = [
 ['5214774444444', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true], 
 ['593968263524', '𝙂𝘽 𝘼𝙨𝙞𝙨𝙩𝙚𝙣𝙘𝙞𝙖 🌻🐈', true],
